329e60950450ce6f56b27f5a05816c9299ddfdad
yoshihide-okabe <hidechan.okabe@gmail.com>
2025-02-04 23:40:52 +0900

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const fetchProduct = async () => {
     try {
-      const requestUrl = `https://tech0-gen8-step4-pos-app-116.azurewebsites.net/${code.trim()}`;
+      const requestUrl = `https://tech0-gen8-step4-pos-app-116.azurewebsites.net/product/${code.trim()}`;
       https://tech0-gen8-step4-pos-app-116.azurewebsites.net/
       console.log("送信URL:", requestUrl);
 
